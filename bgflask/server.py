e4d00cd840bdf78f16d43c1a60459cf7855df483
sookyeongyeom <sookyeong.dev@gmail.com>
2022-02-15 21:15:54 +0900

    server

---
+++ bgflask/server.py
@@ -0,0 +1,38 @@
+from flask import Blueprint, Flask, jsonify
+import pymysql
+import copy
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def send_post():
+    conn = pymysql.connect(
+        user="sky", passwd="7173", host="localhost", db="rss", charset="utf8"
+    )
+    cursor = conn.cursor()
+    query = "SELECT * FROM post_des order by published desc limit 30"
+    cursor.execute(query)
+    result = cursor.fetchall()
+    subject = [
+        "name",
+        "title",
+        "description",
+        "link",
+        "published",
+        "topic",
+        "platform",
+    ]
+    posts = []
+    post = {}
+    for res in result:
+        for i, row in enumerate(res):
+            if i == 0:
+                continue
+            post[subject[i - 1]] = res[i]
+        posts.append(copy.deepcopy(post))
+    return jsonify(posts)
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", port=5000)
